5c67d974ac4775f76166179061e63eeadd3a9bd5
hkaanturgut <kaan.turgut@deop.ca>
2025-12-06 23:46:17 -0500

    feat: Add initial Azure provider configuration in Terraform

---
+++ infra/terraform/providers.tf
@@ -0,0 +1,36 @@
+# Configure the Azure Provider
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~>2.4"
+    }
+  }
+
+  backend "azurerm" {
+    use_oidc             = true
+  }
+}
+
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+
+
+# Get current client configuration
+data "azurerm_client_config" "current" {}
